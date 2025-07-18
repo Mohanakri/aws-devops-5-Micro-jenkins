9d1f2b59c4cb1c04f9289f7f679ff62d95e76941
Mohanakri <96922237+Mohanakri@users.noreply.github.com>
2025-07-18 22:30:30 +0530

    Update App.java

--- src/java-maven-service/src/main/java/com/example/App.java
+++ src/java-maven-service/src/main/java/com/example/App.java
@@ -1,25 +1,24 @@
 # src/java-maven-service/src/main/java/com/example/App.java
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-    
-    @GetMapping("/")
-    public String home() {
-        return "Hi this is from java and maven is build package";
-    }
-    
-    @GetMapping("/health")
-    public String health() {
-        return "OK";
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+
+  @GetMapping("/")
+  public String home() {
+    return "Hi this is from java and maven is build package";
+  }
+
+  @GetMapping("/health")
+  public String health() {
+    return "OK";
+  }
 }
