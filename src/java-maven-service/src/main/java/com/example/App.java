bdaee10894b69f7a9544fbedbe15e7b254146b50
Mohanakri <96922237+Mohanakri@users.noreply.github.com>
2025-07-18 16:48:57 +0530

    Create App.java

---
+++ src/java-maven-service/src/main/java/com/example/App.java
@@ -0,0 +1,25 @@
+# src/java-maven-service/src/main/java/com/example/App.java
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+    
+    @GetMapping("/")
+    public String home() {
+        return "Hi this is from java and maven is build package";
+    }
+    
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
