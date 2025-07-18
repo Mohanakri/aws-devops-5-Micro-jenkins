a4e5d9d905004c5d2a686caa54e1c76e58b892ef
Mohanakri <96922237+Mohanakri@users.noreply.github.com>
2025-07-18 16:55:53 +0530

    Create app.py

---
+++ src/python-service/app.py
@@ -0,0 +1,15 @@
+# src/python-service/app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hi this is from Python"
+
+@app.route('/health')
+def health():
+    return "OK"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
