e3a4096eb11f00ab9bdbd98ad8c6afb8268b995c
Mohanakri <96922237+Mohanakri@users.noreply.github.com>
2025-07-19 20:23:03 +0530

    Update App.java

--- src/java-maven-service/src/main/java/com/example/App.java
+++ src/java-maven-service/src/main/java/com/example/App.java
@@ -1,23 +1,7 @@
 package com.example;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Application implements CommandLineRunner {
-    
+public class App {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-    
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("hi hello this is from java maven");
-        System.out.println("Running on Java " + Runtime.version());
-        System.out.println("Application: java-maven-service");
-        System.out.println("Version: 1.0.0");
-        System.out.println("Java Version: " + System.getProperty("java.version"));
-        System.out.println("Status: Running successfully!");
+        System.out.println("Hello world from Java Maven");
     }
 }
