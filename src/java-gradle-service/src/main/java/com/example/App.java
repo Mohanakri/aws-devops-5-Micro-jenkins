8e5f4bc47adc87c7a58f9283d6b8f73ea91bc925
Mohanakri <96922237+Mohanakri@users.noreply.github.com>
2025-07-19 20:36:57 +0530

    Update App.java

--- src/java-gradle-service/src/main/java/com/example/App.java
+++ src/java-gradle-service/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hi this is java gradle builder");
+        System.out.println("Hello world from Java Gradle");
     }
 }
